d277db6b380e136567a88b93da5bd38b20dde17f
Kris Johnson <kristopher.johnson@ditto.live>
2024-11-19 16:18:54 -0500

    Create iOS app project with Ditto SDK

---
+++ ios/tasks/tasks/tasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  tasksApp.swift
+//  tasks
+//
+//  Created by Kristopher Johnson on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct tasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
