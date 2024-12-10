c6b14274a682e5191d52e62e16b73c3b2594e122
Nick Mosher <nick.mosher@ditto.live>
2024-12-10 10:35:36 -0500

    Switch tabs to spaces

--- javascript/source/cli.js
+++ javascript/source/cli.js
@@ -9,25 +9,25 @@ import { temporaryDirectory } from 'tempy';
 
 const config = dotenv.config({ path: "../.env" });
 const cli = meow(
-	`
-		Usage
-		  $ npm start -- 2>/dev/null
+  `
+    Usage
+      $ npm start -- 2>/dev/null
 
-		Options
-			--app-id [env: DITTO_APP_ID] Your Ditto AppID
-			--playground-token [env: DITTO_PLAYGROUND_TOKEN] An OnlinePlayground token
-	`,
-	{
-		importMeta: import.meta,
-		flags: {
-			appId: {
-				type: "string"
-			},
-			playgroundToken: {
-				type: "string"
-			}
-		}
-	},
+    Options
+      --app-id [env: DITTO_APP_ID] Your Ditto AppID
+      --playground-token [env: DITTO_PLAYGROUND_TOKEN] An OnlinePlayground token
+  `,
+  {
+    importMeta: import.meta,
+    flags: {
+      appId: {
+        type: "string"
+      },
+      playgroundToken: {
+        type: "string"
+      }
+    }
+  },
 );
 
 console.log("Flags:", cli.flags);
@@ -39,19 +39,19 @@ const appID = cli.flags.appId ?? process.env.DITTO_APP_ID;
 const token = cli.flags.playgroundToken ?? process.env.DITTO_PLAYGROUND_TOKEN;
 console.log("Using appId", appID, " and token ", token);
 const ditto = new Ditto({
-	type: "onlinePlayground",
-	appID,
-	token,
+  type: "onlinePlayground",
+  appID,
+  token,
 }, tempdir);
 await ditto.disableSyncWithV3();
 ditto.startSync();
 
 process.on('uncaughtException', (err) => {
-	console.error('Uncaught Exception:', err);
+  console.error('Uncaught Exception:', err);
 });
 
 process.on('unhandledRejection', (reason) => {
-	console.error('Unhandled Rejection:', reason);
+  console.error('Unhandled Rejection:', reason);
 });
 
 render(<App ditto={ditto} />);
