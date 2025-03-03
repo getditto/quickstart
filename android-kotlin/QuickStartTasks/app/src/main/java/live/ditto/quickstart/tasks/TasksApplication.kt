717a25399b8c75df6e30e94c971c4c118d88c4aa
Aaron LaBeau <80424345+biozal@users.noreply.github.com>
2025-03-03 17:14:45 -0600

    Update android-kotlin/QuickStartTasks/app/src/main/java/live/ditto/quickstart/tasks/TasksApplication.kt
    Co-authored-by: Skyler Jokiel <skjokiel@microsoft.com>

--- android-kotlin/QuickStartTasks/app/src/main/java/live/ditto/quickstart/tasks/TasksApplication.kt
+++ android-kotlin/QuickStartTasks/app/src/main/java/live/ditto/quickstart/tasks/TasksApplication.kt
@@ -68,7 +68,7 @@ class TasksApplication : Application() {
         transportConfig.enableAllPeerToPeer()
         ditto.transportConfig = transportConfig
 
-        // disable sync with v3 peers, required for DQL
+        // disable sync with v3 peers, required for syncing with the Ditto Cloud (Big Peer)
         ditto.disableSyncWithV3()
 
         DittoLogger.minimumLogLevel = DittoLogLevel.DEBUG
