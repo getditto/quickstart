3b075d60a9db3e066f15f3b0516d8dfcf563ee81
Aaron LaBeau <alabeau@gmail.com>
2025-03-18 08:52:26 -0500

    updated for Android to remove logging

--- android-kotlin/QuickStartTasks/app/src/main/java/live/ditto/quickstart/tasks/MainActivity.kt
+++ android-kotlin/QuickStartTasks/app/src/main/java/live/ditto/quickstart/tasks/MainActivity.kt
@@ -4,10 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import live.ditto.transports.DittoSyncPermissions
+import android.os.StrictMode
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .penaltyLog() // Log violations to logcat
+                    .build()
+            )
+        }
 
         setContent {
             Root()
