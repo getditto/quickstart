0bbd404204d46dea37285bab28e90da74ac3372f
Ben Chatelain <ben@ditto.com>
2025-06-30 15:08:06 -0600

    refactor: clean up java-spring gradle files

--- java-spring/build.gradle.kts
+++ java-spring/build.gradle.kts
@@ -1,7 +1,3 @@
-import org.apache.tools.ant.taskdefs.condition.Os
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-import org.springframework.boot.gradle.tasks.run.BootRun
-
 plugins {
     id("java")
     id("org.springframework.boot") version "3.4.3"
@@ -19,103 +15,44 @@ java {
     }
 }
 
-repositories {
-    mavenLocal()
-	mavenCentral()
-}
-
-val qaJarBinariesConfiguration = configurations.create("qaJarBinaries") {
-    isCanBeResolved = true
-    isCanBeConsumed = false
-}
-
-val hostJarBinariesConfiguration = configurations.create("hostJarBinaries") {
-    isCanBeResolved = true
-    isCanBeConsumed = false
-}
-
-val requestedQaJarFeatures = project.property("qa-boot-jar-features").toString()
-    .split(",")
-    .map { it.trim() }
-
-val hostBinaryFeature = when {
-    Os.isFamily(Os.FAMILY_MAC) -> when {
-        Os.isArch("aarch64") -> "macos-arm64"
-        Os.isArch("x86_64") -> "macos-x64"
-        else -> {
-            logger.warn("Unsupported macOS architecture. Supported are arm64 and x86_64.")
-            null
-        }
-    }
-    Os.isFamily(Os.FAMILY_WINDOWS) -> when {
-        Os.isArch("x86_64") -> "windows-x64"
-        else -> {
-            logger.warn("Unsupported Windows architecture. Supported is x86_64.")
-            null
-        }
-    }
-    Os.isFamily(Os.FAMILY_UNIX) -> when {
-        Os.isArch("x86_64") -> "linux-x64"
-        else -> {
-            logger.warn("Unsupported Linux architecture. Supported is x86_64.")
-            null
-        }
-    }
-    else -> {
-        logger.warn("Unsupported host platform! Supported are macOS, Windows and Linux.")
-        null
-    }
-}
-
 dependencies {
+    // ditto-java artifact includes the Java API for Ditto
     implementation("com.ditto:ditto-java:5.0.0-preview.1")
     implementation("com.ditto:ditto-internal:5.0.0-preview.1")
 
-    if (hostBinaryFeature != null) {
-        hostJarBinariesConfiguration("com.ditto:ditto-binaries:5.0.0-preview.1") {
+    // This will include binaries for all the supported platforms and architectures
+    implementation("com.ditto:ditto-binaries:5.0.0-preview.1")
+
+    // To reduce your module artifact's size, consider including just the necessary platforms and architectures
+    /*
+        // macOS Apple Silicon
+        implementation("com.ditto:ditto-binaries:5.0.0-preview.1") {
             capabilities {
-                @Suppress("UnstableApiUsage")
-                requireFeature(hostBinaryFeature)
+                requireCapability("com.ditto:ditto-binaries-macos-arm64")
             }
         }
-    }
 
-    requestedQaJarFeatures.forEach { feature ->
-        qaJarBinariesConfiguration("com.ditto:ditto-binaries:5.0.0-preview.1") {
+        // Windows x86_64
+        implementation("com.ditto:ditto-binaries:5.0.0-preview.1") {
             capabilities {
-                @Suppress("UnstableApiUsage")
-                requireFeature(feature)
+                requireCapability("com.ditto:ditto-binaries-windows-x64")
             }
         }
-    }
 
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+        // Linux x86_64
+        implementation("com.ditto:ditto-binaries:5.0.0-preview.1") {
+            capabilities {
+                requireCapability("com.ditto:ditto-binaries-linux-x64")
+            }
+        }
+        */
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("io.projectreactor:reactor-core")
-	runtimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
-
-tasks.named<BootRun>("bootRun") {
-    classpath(hostJarBinariesConfiguration)
-}
-
-tasks.named<BootJar>("bootJar") {
-    classpath(hostJarBinariesConfiguration)
-}
-
-tasks.register<BootJar>("qaBootJar") {
-    description = "Assembles an executable JAR for QA purposes (multiple platforms and architecture support)"
-    group = BasePlugin.BUILD_GROUP
-    classpath(sourceSets.main.get().runtimeClasspath + qaJarBinariesConfiguration)
-    mainClass = "com.ditto.example.spring.quickstart.QuickstartApplication"
-    archiveClassifier = "qa"
-    targetJavaVersion = JavaVersion.VERSION_11
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
