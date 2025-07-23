2bca7e86c99590064c95f52dd17e65c4a220ad60
Aaron LaBeau <alabeau@gmail.com>
2025-07-23 14:25:35 -0500

    Updated React Native to 4.11

--- react-native/App.tsx
+++ react-native/App.tsx
@@ -136,6 +136,10 @@ const App = () => {
         return config;
       });
 
+      // Disable DQL strict mode
+      // https://docs.ditto.live/dql/strict-mode
+      await ditto.current.store.execute('ALTER SYSTEM SET DQL_STRICT_MODE = false');
+
       ditto.current.startSync();
 
       // Register a subscription, which determines what data syncs to this peer
