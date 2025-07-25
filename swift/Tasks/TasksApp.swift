4b4740639c585bfff2bb17a80e8c5c317047583f
Konstantin Bender <me@konstantinbender.com>
2025-07-25 10:04:18 +0200

    Set strict mode to false on app start

--- swift/Tasks/TasksApp.swift
+++ swift/Tasks/TasksApp.swift
@@ -2,9 +2,17 @@ import SwiftUI
 
 @main
 struct TasksApp: App {
+    let ditto = DittoManager.shared.ditto
+
     var body: some Scene {
         WindowGroup {
-            TasksListScreen()
+            TasksListScreen().task {
+                do {
+                    try await ditto.store.execute(query: "ALTER SYSTEM SET DQL_STRICT_MODE = false")
+                } catch {
+                    fatalError("Internal inconsistency, expected setting DQL strict mode to false to always succeed but it failed: \(error)")
+                }
+            }
         }
     }
 }
